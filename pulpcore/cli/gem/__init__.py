28e9c7f16b4581f97c753e7a63b3e084f7e2a948
Matthias Dellweg <mdellweg@redhat.com>
2023-07-03 10:38:50 +0200

    Release 0.1.0
    [noissue]

--- pulpcore/cli/gem/__init__.py
+++ pulpcore/cli/gem/__init__.py
@@ -13,7 +13,7 @@ from pulpcore.cli.gem.repository import repository
 translation = get_translation(__name__)
 _ = translation.gettext
 
-__version__ = "0.0.0.dev"
+__version__ = "0.1.0"
 
 
 @pulp_group(name="gem")
