5bccc8381e1c1c42cefc31f0793a01d28b4a0ce8
Matthias Dellweg <mdellweg@redhat.com>
2023-09-05 13:22:48 +0200

    Update CI
    [noissue]

--- pulp-glue-gem/pulp_glue/gem/context.py
+++ pulp-glue-gem/pulp_glue/gem/context.py
@@ -20,7 +20,7 @@ class PulpGemContentContext(PulpContentContext):
     HREF = "gem_gem_content_href"
     ID_PREFIX = "content_gem_gem"
     CAPABILITIES = {"upload": []}
-    NEEDS_PLUGINS = [PluginRequirement("gem", min="0.0.0")]
+    NEEDS_PLUGINS = [PluginRequirement("gem")]
 
 
 class PulpGemDistributionContext(PulpDistributionContext):
@@ -30,7 +30,7 @@ class PulpGemDistributionContext(PulpDistributionContext):
     ENTITIES = _("gem distributions")
     HREF = "gem_gem_distribution_href"
     ID_PREFIX = "distributions_gem_gem"
-    NEEDS_PLUGINS = [PluginRequirement("gem", min="0.0.0")]
+    NEEDS_PLUGINS = [PluginRequirement("gem")]
 
     def preprocess_body(self, body: EntityDefinition) -> EntityDefinition:
         body = super().preprocess_body(body)
@@ -48,7 +48,7 @@ class PulpGemPublicationContext(PulpPublicationContext):
     ENTITIES = _("gem publications")
     HREF = "gem_gem_publication_href"
     ID_PREFIX = "publications_gem_gem"
-    NEEDS_PLUGINS = [PluginRequirement("gem", min="0.0.0")]
+    NEEDS_PLUGINS = [PluginRequirement("gem")]
 
     def preprocess_entity(self, body: EntityDefinition, partial: bool = False) -> EntityDefinition:
         body = super().preprocess_entity(body, partial=partial)
@@ -66,7 +66,7 @@ class PulpGemRemoteContext(PulpRemoteContext):
     ENTITIES = _("gem remotes")
     HREF = "gem_gem_remote_href"
     ID_PREFIX = "remotes_gem_gem"
-    NEEDS_PLUGINS = [PluginRequirement("gem", min="0.0.0")]
+    NEEDS_PLUGINS = [PluginRequirement("gem")]
 
 
 class PulpGemRepositoryVersionContext(PulpRepositoryVersionContext):
@@ -74,7 +74,7 @@ class PulpGemRepositoryVersionContext(PulpRepositoryVersionContext):
     RESOURCE_TYPE = "gem"
     HREF = "gem_gem_repository_version_href"
     ID_PREFIX = "repositories_gem_gem_versions"
-    NEEDS_PLUGINS = [PluginRequirement("gem", min="0.0.0")]
+    NEEDS_PLUGINS = [PluginRequirement("gem")]
 
 
 class PulpGemRepositoryContext(PulpRepositoryContext):
@@ -83,5 +83,5 @@ class PulpGemRepositoryContext(PulpRepositoryContext):
     HREF = "gem_gem_repository_href"
     ID_PREFIX = "repositories_gem_gem"
     VERSION_CONTEXT = PulpGemRepositoryVersionContext
-    NEEDS_PLUGINS = [PluginRequirement("gem", min="0.0.0")]
+    NEEDS_PLUGINS = [PluginRequirement("gem")]
     NULLABLES = PulpRepositoryContext.NULLABLES | {"remote"}
