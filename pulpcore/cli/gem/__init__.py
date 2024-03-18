480b4aad4d66fa3f5fad09fdbcf47a36f3e8f20b
pulpbot <pulp-infra@redhat.com>
2024-03-18 12:04:39 +0000

    Release 0.4.0

--- pulpcore/cli/gem/__init__.py
+++ pulpcore/cli/gem/__init__.py
@@ -13,7 +13,7 @@ from pulpcore.cli.gem.repository import repository
 translation = get_translation(__package__)
 _ = translation.gettext
 
-__version__ = "0.4.0.dev"
+__version__ = "0.4.0"
 
 
 @pulp_group(name="gem")
