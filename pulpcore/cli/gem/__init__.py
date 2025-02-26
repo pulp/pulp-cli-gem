e8b74423b489306069dc851923bb6cc159265237
pulpbot <pulp-infra@redhat.com>
2025-02-26 13:03:36 +0000

    Bump version: 0.5.2 → 0.5.3.dev

--- pulpcore/cli/gem/__init__.py
+++ pulpcore/cli/gem/__init__.py
@@ -13,7 +13,7 @@ from pulpcore.cli.gem.repository import repository
 translation = get_translation(__package__)
 _ = translation.gettext
 
-__version__ = "0.5.2"
+__version__ = "0.5.3.dev"
 
 
 @pulp_group(name="gem")
